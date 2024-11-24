09d34812107e49a1d3921ca61c78e214a6080b4e
Rodrigo <lucianorodrigocarvalho@gmail.com>
2024-11-24 18:43:30 -0300

    Created project

---
+++ src/main/java/com/smartprocess/spmodeloDominio/SpmodeloDominioApplication.java
@@ -0,0 +1,13 @@
+package com.smartprocess.spmodeloDominio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpmodeloDominioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpmodeloDominioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartprocess/spmodeloDominio/SpmodeloDominioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartprocess.spmodeloDominio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpmodeloDominioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
